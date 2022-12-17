db9edb6dafb3a6fa10d3d0ad5d774411c2a2696a
mayank.tomar <mayank.tomar@u-next.com>
2022-12-17 19:04:04 +0530

    Initial commit

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,13 +1,21 @@
 import { buildQueries } from '@testing-library/react';
 import React from 'react';
-import {Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Spinner} from 'reactstrap';
+import {Card, CardBody, CardTitle, Form, FormGroup, Label, Input, CardSubtitle, CardText, Button, Spinner, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import { dishDetails } from '../dishDetails';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-export default function DishDetails() {
+export default function DishDetails(props) {
 
   const [dishes,setDishes] = useState([]);
   const [isLoading,SetIsLoading] = useState(true);
+  const [selectedDish,setSelectedDish] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [title,setTitle] = useState('');
+  const [description,setDescription] = useState('');
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  }
 
   useEffect(() => {
     axios.get('/dishes')
@@ -22,6 +30,19 @@ export default function DishDetails() {
     })
   }, [])
   
+  const fetchDishes = async () => {
+    SetIsLoading(true);
+    axios.get('/dishes')
+    .then((response)=>{
+      setTimeout(()=>{
+        SetIsLoading(false);
+        setDishes(response.data);
+      },5000)
+    })
+    .catch((error)=>{
+      console.log("Api error",error);
+    })
+  }
 
   const styling = {
     heading : {
@@ -33,7 +54,20 @@ export default function DishDetails() {
     }
   }
 
-  
+  const onValueChange = (event) => {
+    const value = event.currentTarget.value;
+    if (event.currentTarget.name=='title') {
+      setTitle(value);
+    } else {
+      setDescription(value);
+    }
+  }
+
+  const onModalClose = () => {
+
+    toggleModal();
+   fetchDishes();
+  }
   const displayDetails = () => {
     return(
       dishes.map((dishDetail)=>{
@@ -56,8 +90,11 @@ export default function DishDetails() {
           <CardText>
             {dishDetail.description}
           </CardText>
-          <Button>
-            <a href={dishDetail.url}>Click</a>
+          <Button onClick={()=>{
+            setSelectedDish(dishDetail);
+            toggleModal();
+          }}>
+            Click
           </Button>
         </CardBody>
       </Card>
@@ -76,6 +113,46 @@ export default function DishDetails() {
         <p style={styling.paragraph}>Here are details of our popluar dishes</p>
         {isLoading ? <Spinner>Loading...</Spinner> : displayDetails()}
       </div>
+
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Modal title</ModalHeader>
+        <ModalBody>
+        <Form >
+    <FormGroup>
+        <Label for="exampleName">
+          Title
+        </Label>
+        <Input
+          id="exampleName"
+          name="title"
+          defaultValue={selectedDish.title}
+          type="text"
+          onChange={onValueChange}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleEmail">
+        Description
+        </Label>
+        <Input
+          id="exampleEmail"
+          name="description"
+          defaultValue={selectedDish.description}
+          type="text"
+          onChange={onValueChange}
+        />
+      </FormGroup>
+      </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={onModalClose}>
+            Do Something
+          </Button>{' '}
+          <Button color="secondary" onClick={toggleModal}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   )
 }
