10b412ecd0668fbef2d6629e6ef6adeef3703bce
mayank.tomar <mayank.tomar@u-next.com>
2022-12-18 15:49:26 +0530

    Put and delete api implementation

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,6 +1,6 @@
 import { buildQueries } from '@testing-library/react';
 import React from 'react';
-import {Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Spinner} from 'reactstrap';
+import {Card, CardBody, CardTitle, Form, FormGroup, Label, Input, CardSubtitle, CardText, Button, Spinner, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import { dishDetails } from '../dishDetails';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -8,6 +8,10 @@ export default function DishDetails() {
 
   const [dishes,setDishes] = useState([]);
   const [isLoading,SetIsLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedDish, setSelectedDish] = useState({});
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   useEffect(() => {
     axios.get('/dishes')
@@ -15,13 +19,37 @@ export default function DishDetails() {
       setTimeout(()=>{
         SetIsLoading(false);
         setDishes(response.data);
-      },5000)
+      },1000)
     })
     .catch((error)=>{
       console.log("Api error",error);
     })
   }, [])
   
+  const fetchDishes = () => {
+    SetIsLoading(true);
+    axios.get('/dishes')
+    .then((response)=>{
+      setTimeout(()=>{
+        SetIsLoading(false);
+        setDishes(response.data);
+      },100)
+    })
+    .catch((error)=>{
+      console.log("Api error",error);
+    })
+  }
+
+  const updateDishesState = () => {
+    const dishesToChange = dishes; //creating a copy of dishes state
+    const index = dishes.indexOf(selectedDish); //finding index number of selected dish inside dishes
+    selectedDish.title = title; //updating selected dish with new title
+    selectedDish.description = description; //updating selected dish with new description
+
+    dishesToChange[index] = selectedDish; //Updating the element at the repective index number with the new object
+
+    setDishes(dishesToChange); //updating dishes state with the changed copy
+  }
 
   const styling = {
     heading : {
@@ -33,6 +61,50 @@ export default function DishDetails() {
     }
   }
 
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  }
+
+  const updateDish = () => {
+    console.log("Selected Dish",selectedDish);
+    console.log("Title",title);
+    console.log("Description",description);
+    axios.put('/updateDish/'+selectedDish._id,{
+      title: title,
+      description: description
+    })
+    .then((response)=>{
+      console.log(response);
+      toggleModal();
+      updateDishesState();
+    })
+    .catch((error)=>{
+      console.log(error);
+      alert(error);
+    })
+    
+  }
+
+  const deleteDish = () => {
+    axios.delete('/deleteDish/'+selectedDish._id)
+    .then((response)=>{
+      console.log(response);
+      fetchDishes();
+    })
+    .catch((error)=>{
+      console.log(error);
+      alert(error);
+    })
+  }
+
+  const onValueChange = (event) => {
+    const value = event.target.value;
+    if (event.target.name=='title') {
+      setTitle(value);
+    } else {
+      setDescription(value);
+    }
+  }
   
   const displayDetails = () => {
     return(
@@ -56,9 +128,19 @@ export default function DishDetails() {
           <CardText>
             {dishDetail.description}
           </CardText>
-          <Button>
-            <a href={dishDetail.url}>Click</a>
+          <Button onClick={()=>{
+            setSelectedDish(dishDetail)
+            setTitle(dishDetail.title)
+            setDescription(dishDetail.description)
+            toggleModal();
+          }}>
+            Update
           </Button>
+
+          <Button onClick={()=>{
+            setSelectedDish(dishDetail)
+            deleteDish();
+          }}>Delete</Button>
         </CardBody>
       </Card>
         </div>
@@ -76,6 +158,48 @@ export default function DishDetails() {
         <p style={styling.paragraph}>Here are details of our popluar dishes</p>
         {isLoading ? <Spinner>Loading...</Spinner> : displayDetails()}
       </div>
+
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Modal title</ModalHeader>
+        <ModalBody>
+        <Form >
+          <FormGroup>
+                <Label for="exampleName">
+                  Title
+                </Label>
+                <Input
+                  id="exampleName"
+                  name="title"
+                  placeholder="with a placeholder"
+                  type="text"
+                  defaultValue={selectedDish.title}
+                  onChange={onValueChange}
+                />
+          </FormGroup>
+          <FormGroup>
+                <Label for="exampleName">
+                  Description
+                </Label>
+                <Input
+                  id="exampleName"
+                  name="description"
+                  placeholder="with a placeholder"
+                  type="text"
+                  defaultValue={selectedDish.description}
+                  onChange={onValueChange}
+                />
+          </FormGroup>
+      </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={updateDish}>
+            Update
+          </Button>{' '}
+          <Button color="secondary" onClick={toggleModal}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   )
 }
